4f4c65f141c1149fb9055490ca0e921cdd57ef5c
GauravSinghRauthan <gauravsingh199889@gmail.com>
2023-07-19 17:56:32 +0530

    80% work done

---
+++ 08/index.js
@@ -0,0 +1,12 @@
+const { subscribe } = require('diagnostics_channel');
+const EventEmitter = require('events');
+
+const eventEmitter = new EventEmitter();
+
+const subscribeMeg = (channel)=>{
+    console.log(`Thanks for subscribing to ${channel}`)
+}
+
+eventEmitter.addListener("subscribe",subscribeMeg)
+
+eventEmitter.emit("subscribe","College Wallah")
